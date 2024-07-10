79fa79af85ada29e9be270a0806926f5b6c06149
Brandon Lee <brandon.sangmin.lee@gmail.com>
2024-07-10 10:51:36 -0500

    Displaying dates instead of UNIX timestamps

--- ML_strategy/strategies.py
+++ ML_strategy/strategies.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 import trading_env
+import numpy as np
 
-# Warning suppression
+# === WARNING SUPPRESSION ===
+
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -15,7 +17,21 @@ def suppress_stdout_stderr():
         with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
             yield (err, out)
 
-# Strategies
+# === HELPER METHODS ===
+
+# returns True if seq1 crosses under seq2 at the index
+def cross_under(seq1, seq2, index):
+    if index < 1: return False
+    seq1 = np.array(seq1)
+    seq2 = np.array(seq2)
+    return seq1[index] < seq2[index] and seq1[index-1] >= seq2[index-1]
+
+# ema of a pandas dataframe
+def ema(seq, lifetime, times):
+    return seq.ewm(halflife=f"{lifetime/2} days", times=times).mean()
+
+# === STRATEGIES ===
+
 class Strategy(ABC):
     def __init__(self):
         pass
@@ -43,3 +59,44 @@ class BuyAndHoldStrategy(Strategy):
 class DoNothingStrategy(Strategy):
     def get_action(self, state):
         return [0]
+    
+# sell % when slope crosses from positive to negative, buy when vice versa
+class RegressionSlopeStrategy(Strategy):
+
+    def __init__(self, length, percentage):
+        self.length = length
+        self.percentage = percentage
+        self.prev_slope = None
+
+    def get_action(self, state):
+        state_dict = trading_env.unflatten_dictarr(state)
+        close = state_dict["close"][-self.length:]
+        indices = np.arange(self.length)
+        slope = np.sum((close - np.mean(close)) * (indices - np.mean(indices))) / np.sum((indices - np.mean(indices))**2)
+
+        action = [0]
+        if self.prev_slope is not None:
+            # print(slope)
+            if slope < 0 and self.prev_slope >= 0:
+                action = [-self.percentage * state_dict["long"]]
+            elif slope > 0 and self.prev_slope <= 0:
+                action = [self.percentage * state_dict["cash"] / (state_dict["close"][-1])]
+        self.prev_slope = slope
+        return action
+
+# class JinStrategy(Strategy):
+#     def __init__(self,
+#                  ema_fast=20, ema_slow=60, ema_superslow=120,
+#                  rsi_threshold=70,
+#                  slow_sell=0.01, quick_sell=0.05,
+#                  tdfi_thresh=-100):
+#             self.ema_fast = ema_fast
+#             self.ema_slow = ema_slow
+#             self.ema_superslow = ema_superslow
+#             self.rsi_threshold = rsi_threshold
+#             self.slow_sell = slow_sell
+#             self.quick_sell = quick_sell
+#             self.tdfi_thresh = tdfi_thresh
+#     def get_action(self, state):
+#         state_dict = trading_env.unflatten_dictarr(state)
+        
